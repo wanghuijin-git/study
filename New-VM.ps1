57522772a2c597f50b4799a88ac39741c47b37d1
jichengxi <43433188+jichengxi@users.noreply.github.com>
2019-08-19 14:48:32 +0800

    Update New-VM.ps1

--- New-VM.ps1
+++ New-VM.ps1
@@ -59,25 +59,25 @@ while ("1" -eq "1")
                 {
                     Get-VM $VMconfig.VMName |Start-VM -EA SilentlyContinue | Out-Null
                     
-                    Start-Sleep -Seconds 10
-                    $DelNet2GayWay="sed -i '/^GATEWAY/'d /etc/sysconfig/network-scripts/ifcfg-ens224"
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $DelNet2GayWay -Confirm:$false | Out-Null
+                    # Start-Sleep -Seconds 10
+                    # $DelNet2GayWay="sed -i '/^GATEWAY/'d /etc/sysconfig/network-scripts/ifcfg-ens224"
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $DelNet2GayWay -Confirm:$false | Out-Null
                                     
-                    $Net2IP="$VMconfig.IpAddress2"
-                    $SetNet2Ip="sed -i '/^IPADDR=/c\IPADDR=$Net2IP' /etc/sysconfig/network-scripts/ifcfg-ens224"
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Ip -Confirm:$false | Out-Null
+                    # $Net2IP="$VMconfig.IpAddress2"
+                    # $SetNet2Ip="sed -i '/^IPADDR=/c\IPADDR=$Net2IP' /etc/sysconfig/network-scripts/ifcfg-ens224"
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Ip -Confirm:$false | Out-Null
                     
                     # $IPSPLIT=$Net2IP -split '\.'
                     # $Net2GateWay=$IPSPLIT[0]+"."+$IPSPLIT[1]+"."+$IPSPLIT[2]+".254"
-                    $Net2GateWay=$VMconfig.GateWay2
-                    $SetNet2RouteClean="echo '' > /etc/sysconfig/network-scripts/route-ens224"
-                    $SetNet2Route1="echo '172.22.0.0/16 via $Net2GateWay' > /etc/sysconfig/network-scripts/route-ens224"
-                    $SetNet2Route2="echo '172.30.0.0/16 via $Net2GateWay' >> /etc/sysconfig/network-scripts/route-ens224"
-                    $SetNet2Route3="echo '172.29.0.0/16 via $Net2GateWay' >> /etc/sysconfig/network-scripts/route-ens224"
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2RouteClean -Confirm:$false | Out-Null
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route1 -Confirm:$false | Out-Null
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route2 -Confirm:$false | Out-Null
-                    Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route3 -Confirm:$false | Out-Null
+                    # $Net2GateWay=$VMconfig.GateWay2
+                    # $SetNet2RouteClean="echo '' > /etc/sysconfig/network-scripts/route-ens224"
+                    # $SetNet2Route1="echo '172.22.0.0/16 via $Net2GateWay' > /etc/sysconfig/network-scripts/route-ens224"
+                    # $SetNet2Route2="echo '172.30.0.0/16 via $Net2GateWay' >> /etc/sysconfig/network-scripts/route-ens224"
+                    # $SetNet2Route3="echo '172.29.0.0/16 via $Net2GateWay' >> /etc/sysconfig/network-scripts/route-ens224"
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2RouteClean -Confirm:$false | Out-Null
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route1 -Confirm:$false | Out-Null
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route2 -Confirm:$false | Out-Null
+                    # Invoke-VMScript -VM $VMconfig.VMName -ScriptType Bash -GuestUser root -GuestPassword "Bestpay!2016" -ScriptText $SetNet2Route3 -Confirm:$false | Out-Null
                 }
             }
         }
